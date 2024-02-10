af448a8b791c7fb74a6b48876ce6dccc4e33a62e
Shubham <sm075174@gmail.com>
2024-02-11 00:12:09 +0530

    page1 or hero section is done

--- script.js
+++ script.js
@@ -46,6 +46,15 @@ function loadingAnimatio() {
   tl.to(".loader", {
     display: "none",
   });
+
+  tl.from(".nav", {
+    opacity: 0,
+  });
+
+  tl.from("#hero1 h1, #hero2 h1, #hero3 h2, #hero4 h1", {
+    y: 120,
+    stagger: 0.2,
+  });
 }
 
 function cursorAnimation() {
