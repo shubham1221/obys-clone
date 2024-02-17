88126bd7d9ac875725f90c8ba9260a8e5b9e5e0c
Shubham <sm075174@gmail.com>
2024-02-18 00:34:41 +0530

    page2 vedio section and page3 project section are done

--- script.js
+++ script.js
@@ -95,25 +95,85 @@ function loadingAnimatio() {
     y: 150,
     stagger: 0.2,
   });
+
+  // tl.from(
+  //   "#hero1, .page2 ",
+  //   {
+  //     opacity: 0,
+  //   },
+  //   "-=1"
+  // );
 }
 
 function cursorAnimation() {
-  document.addEventListener("mousemove", (dets) => {
-    gsap.to("#crsr", {
-      left: dets.x,
-      top: dets.y,
-    });
+  // For custom cursor
+  // document.addEventListener("mousemove", (dets) => {
+  //   gsap.to("#crsr", {
+  //     left: dets.x,
+  //     top: dets.y,
+  //   });
+  // });
+
+  Shery.mouseFollower({
+    skew: true,
+    eae: "cubic-bazier(0.23, 1, 0.320, 1)",
+    duration: 1,
   });
 
   // Make Menu Magnet
 
-  Shery.makeMagnet(".nav-part2 h4" /* Element to target.*/, {
-    //Parameters are optional.
-    // ease: "cubic-bezier(0.23, 1, 0.320, 1)",
-    // duration: 1,
+  Shery.makeMagnet(".nav-part2 h4", {});
+
+  // let videoContainer = document.querySelector(".vedio-container");
+  // videoContainer.addEventListener("mouseenter", () => {
+  //   videoContainer.addEventListener("mousemove", (dets) => {
+  //     gsap.to("#video-cursor", {
+  //       left: dets.x,
+  //       y: dets.y,
+  //     });
+  //   });
+  // });
+}
+
+function sheryAnimation() {
+  Shery.imageEffect(".image-div", {
+    style: 5,
+    config: {
+      a: { value: 2, range: [0, 30] },
+      b: { value: 0.75, range: [-1, 1] },
+      zindex: { value: "9996999", range: [-9999999, 9999999] },
+      aspect: { value: 0.7272749932567818 },
+      ignoreShapeAspect: { value: true },
+      shapePosition: { value: { x: 0, y: 0 } },
+      shapeScale: { value: { x: 0.5, y: 0.5 } },
+      shapeEdgeSoftness: { value: 0, range: [0, 0.5] },
+      shapeRadius: { value: 0, range: [0, 2] },
+      currentScroll: { value: 0 },
+      scrollLerp: { value: 0.07 },
+      gooey: { value: true },
+      infiniteGooey: { value: false },
+      growSize: { value: 4, range: [1, 15] },
+      durationOut: { value: 1, range: [0.1, 5] },
+      durationIn: { value: 1.5, range: [0.1, 5] },
+      displaceAmount: { value: 0.5 },
+      masker: { value: true },
+      maskVal: { value: 1.73, range: [1, 5] },
+      scrollType: { value: 0 },
+      geoVertex: { range: [1, 64], value: 1 },
+      noEffectGooey: { value: true },
+      onMouse: { value: 0 },
+      noise_speed: { value: 0.2, range: [0, 10] },
+      metaball: { value: 0.49, range: [0, 2] },
+      discard_threshold: { value: 0.5, range: [0, 1] },
+      antialias_threshold: { value: 0, range: [0, 0.1] },
+      noise_height: { value: 0.34, range: [0, 2] },
+      noise_scale: { value: 10, range: [0, 100] },
+    },
+    gooey: true,
   });
 }
 
 loadingAnimatio();
 cursorAnimation();
 locomotiveAnimation();
+sheryAnimation();
